3951e0813bc66e2967ca45dec6b06987bab73979
SamarpitSantoki <samarpit.santoki@gmail.com>
2022-01-25 10:06:03 +0530

    Minor fixes to responsiveness

--- components/Navs/Nav.tsx
+++ components/Navs/Nav.tsx
@@ -7,31 +7,37 @@ const Nav = () => {
     let state = document.getElementById("dropdown").className;
     let btn = document.getElementById("toggler");
 
-    if (state == "hidden md:hidden") {
-      document.getElementById("dropdown").className = "flex md:hidden";
-      btn.className = "bg-[#34A19C] rounded-md md:hidden";
+    if (state == "hidden sm:hidden") {
+      document.getElementById("dropdown").className = "flex sm:hidden";
+      btn.className = "bg-[#34A19C] rounded-md sm:hidden";
     } else {
-      document.getElementById("dropdown").className = "hidden md:hidden";
+      document.getElementById("dropdown").className = "hidden sm:hidden";
       btn.className =
-        "bg-[#00a59c] rounded-md shadow-sm shadow-gray-500 md:hidden";
+        "bg-[#00a59c] rounded-md shadow-sm shadow-gray-500 sm:hidden";
     }
   }
 
   return (
     <>
       <div className="bg-medi w-100 px-6 h-20  flex justify-between items-center overflow-hidden">
-        <img className="h-12" src="./images/logo.jpg" alt="" />
-        <div className="md:flex md:w-2/5 hidden ">
-          <input
-            className="w-5/6 rounded-sm focus:outline focus:outline-2 focus:outline-offset-0 py-1 px-2 focus:outline-blue-500 placeholder:italic "
-            type="search"
-            name="find_prod"
-            id="prod_search"
-            placeholder="Search"
+        <div className="w-3/4 space-x-4 inline-flex justify-start items-center">
+          <img
+            className="h-12 w-auto aspect-auto"
+            src="./images/logo.jpg"
+            alt=""
           />
-          <button className="flex items-center uppercase font-light text-sm h-8 text-green-300 justify-center mx-2 px-5 py-2 outline outline-2 rounded outline-green-500">
-            Search
-          </button>
+          <div className="sm:flex sm:w-full hidden ">
+            <input
+              className="w-5/6 rounded-sm focus:outline focus:outline-2 focus:outline-offset-0 py-1 px-2 focus:outline-blue-500 placeholder:italic "
+              type="search"
+              name="find_prod"
+              id="prod_search"
+              placeholder="Search"
+            />
+            <button className="flex items-center uppercase font-light text-sm h-8 text-green-300 justify-center mx-2 px-5 py-2 outline outline-2 rounded outline-green-500">
+              Search
+            </button>
+          </div>
         </div>
         <div className="flex space-x-3 text-white text-[1.05rem] items-center">
           <a className="inline-flex" href="#">
@@ -45,13 +51,13 @@ const Nav = () => {
           <button
             onClick={dropdownClick}
             id="toggler"
-            className="bg-[#00a59c] rounded-md shadow-sm shadow-gray-500 md:hidden"
+            className="bg-[#00a59c] rounded-md shadow-sm shadow-gray-500 sm:hidden"
           >
             <Menu className="w-10" color="white" />
           </button>
         </div>
       </div>
-      <div id="dropdown" className="hidden md:hidden">
+      <div id="dropdown" className="hidden sm:hidden">
         <div className="flex bg-medi w-full p-3 mx-auto">
           <input
             className="w-5/6 rounded-sm focus:outline focus:outline-2 focus:outline-offset-0 py-1 px-2 focus:outline-blue-500 placeholder:italic "
