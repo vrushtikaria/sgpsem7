bea6e28f0362fa3c501f1c90dd7587f576305f9f
SamarpitSantoki <samarpit.santoki@gmail.com>
2022-02-05 11:12:36 +0530

    Added Product fetching and responsiveness

--- pages/api/prodbycat/index.ts
+++ pages/api/prodbycat/index.ts
@@ -3,16 +3,13 @@ import Product from "../../../models/productSchema";
 
 export default async function handler(req, res) {
   await dbConnect();
-
   const cate = req.query.cat.toString();
-
   const result = await Product.find({ category: cate }).limit(10);
-
   const prods = result.map((doc) => {
     const prod = doc.toObject();
     prod._id = prod._id.toString();
     prod.image = "/images/product_images/" + prod._id + "/" + prod.image;
     return prod;
   });
-  res.status(202).send(prods);
+  res.status(202).json(prods);
 }
